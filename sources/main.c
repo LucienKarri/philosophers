6c6fb728635be768f4508549e830fed3ca1f64cc
LucienKarri <lkarri@student.21-school.ru>
2021-10-26 13:10:13 +0300

    error out from write and fd(2)

--- sources/main.c
+++ sources/main.c
@@ -6,7 +6,7 @@
 /*   By: lkarri <lkarri@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/22 15:44:55 by lkarri            #+#    #+#             */
-/*   Updated: 2021/10/26 10:57:48 by lkarri           ###   ########.fr       */
+/*   Updated: 2021/10/26 12:56:19 by lkarri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,17 +20,17 @@ int	main(int argc, char **argv)
 	i = 0;
 	if (argc != 5 && argc != 6)
 	{
-		printf("Wrong number of arguments\n");
-		return (-1);
+		write(2, "Error: wrong number of arguments\n", 34);
+		return (1);
 	}
 	i = parse_and_init(&data, argv);
 	if (i != 0)
 	{
 		if (i == 1)
-			printf("All variables must be positive\n");
+			write(2, "Error: all variables must be positive\n", 39);
 		if (i == 2)
-			printf("Too few philosophers\n");
-		return (-1);
+			write(2, "Error: too few philosophers\n", 29);
+		return (1);
 	}
 	simulation (&data);
 	free(data.fork);
